984eec77974400478e3415ac5f8a1552c0f30e6d
Vova <test@gmail.com>
2025-01-22 11:16:28 -0800

    firs commit after 2 years

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// const result = confirm('Are you here?')
+// const answer = prompt("Are you 18?", "")
+// console.log(typeof(answer))
+
+const answers = [];
+
+answers[0] = prompt('What is Your Name?', '');
+answers[1] = prompt('What is Your SurName?', '');
+answers[3] = prompt('How old are You?', '');
+
+console.log(typeof(answers))

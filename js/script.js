9c7cc63d0607aa7a1ced584823038940a46c725f
Vova <test@gmail.com>
2025-01-23 01:52:56 -0800

    something with js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@
 // const answer = prompt("Are you 18?", "")
 // console.log(typeof(answer))
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('What is Your Name?', '');
-answers[1] = prompt('What is Your SurName?', '');
-answers[3] = prompt('How old are You?', '');
+// answers[0] = prompt('What is Your Name?', '');
+// answers[1] = prompt('What is Your SurName?', '');
+// answers[3] = prompt('How old are You?', '');
 
-console.log(typeof(answers))
+// console.log(typeof(answers))
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10;
+let dcr  = 10;
+
+incr++;
+dcr--;
